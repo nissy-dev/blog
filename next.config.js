7ba2831ed7f02c61bddae5b8a94aed8d79cdf403
nissy-dev <nd.12021218@gmail.com>
2021-09-08 21:44:29 +0900

    fix(ci): fix deploy

--- next.config.js
+++ next.config.js
@@ -2,23 +2,13 @@ const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 const { i18n } = require("./next-i18next.config");
 
-// module.exports = withPWA({
-//   i18n,
-//   pwa: {
-//     dest: "public",
-//     runtimeCaching,
-//   },
-//   experimental: {
-//     esmExternals: true,
-//   },
-// });
-module.exports = {
+module.exports = withPWA({
   i18n,
-  // pwa: {
-  //   dest: "public",
-  //   runtimeCaching,
-  // },
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
   experimental: {
     esmExternals: true,
   },
-};
+});
