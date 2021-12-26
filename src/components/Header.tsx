a4cee97c9028f25aa51e476f0e46a411d41eaf4b
nissy-dev <nd.12021218@gmail.com>
2021-12-26 12:40:01 +0900

    feat(search): use docsearch

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,16 @@
-import { useState } from "react";
 import { css } from "@emotion/react";
-import dynamic from "next/dynamic";
+import { DocSearch } from "@docsearch/react";
+
+import "@docsearch/css";
 
 import { Link } from "components/Link";
 import { ProfileButton } from "components/ProfileButton";
-import { SearchButton } from "components/SearchButton";
 import { ThemeSwitchButton } from "components/ThemeSwitchButton";
-import type { Props as SearchBoxProps } from "components/Search";
-import { siteMetadata } from "utils/const";
+import { siteMetadata, ALGOLIA_APP_ID, ALGOLIA_SEARCH_KEY, ALGOLIA_INDEX_NAME } from "utils/const";
 import { useTranslation } from "utils/useTranslation";
-
-const SearchBoxComponent = dynamic<SearchBoxProps>(() =>
-  import("components/Search").then((modules) => modules.Search)
-);
 
 export const Header = () => {
   const { t } = useTranslation();
-  const [showSearchBox, setShowSearchBox] = useState(false);
-  const handleSearchBox = () => setShowSearchBox(!showSearchBox);
 
   return (
     <header css={headerStyle}>
@@ -26,10 +19,13 @@ export const Header = () => {
           <h1>{siteMetadata.title}</h1>
         </Link>
         <ProfileButton />
-        <SearchButton showSearchBox={showSearchBox} onClick={handleSearchBox} />
         <ThemeSwitchButton />
+        <DocSearch
+          appId={ALGOLIA_APP_ID}
+          indexName={ALGOLIA_INDEX_NAME}
+          apiKey={ALGOLIA_SEARCH_KEY}
+        />
       </div>
-      <div>{showSearchBox && <SearchBoxComponent handleSearchBox={handleSearchBox} />}</div>
     </header>
   );
 };
