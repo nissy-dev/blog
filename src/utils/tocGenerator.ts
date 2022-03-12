348b014079d062dd98d9b7c5d18b7761c8478ec4
nissy-dev <nd.12021218@gmail.com>
2022-03-12 18:09:12 +0900

    fix: fix remark

--- src/utils/tocGenerator.ts
+++ src/utils/tocGenerator.ts
@@ -1,9 +1,7 @@
 import toc from "markdown-toc";
 import rehypeStringify from "rehype-stringify";
-import remarkCore from "remark";
+import { remark } from "remark";
 import remark2Rehype from "remark-rehype";
-
-const { remark } = remarkCore;
 
 export const tocGenerator = async (content: string): Promise<string> => {
   const extractTocMarkdown = toc(content, { maxdepth: 3 }).content;
