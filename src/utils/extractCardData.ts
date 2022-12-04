fd17229c035f852915f944b19ef7455d4dc13ecd
nissy-dev <nd.12021218@gmail.com>
2022-12-04 14:34:58 +0900

    fix: resolve build error

--- src/utils/extractCardData.ts
+++ src/utils/extractCardData.ts
@@ -1,4 +1,4 @@
-import ogs, { OpenGraphImage, SuccessResult } from "open-graph-scraper";
+import ogs from "open-graph-scraper";
 
 export type CardData = {
   title: string;
@@ -9,32 +9,50 @@ export type CardData = {
   url: string;
 };
 
-const cardDataValidator = (data: { [key: string]: string | undefined }): data is CardData => {
-  return !!data.title && !!data.description && !!data.hostname && !!data.favicon;
+type OgsResultType = {
+  ogTitle: string;
+  ogDescription: string;
+  ogImage: {
+    url: string;
+  };
+};
+
+const isOgsResultType = (result: object): result is OgsResultType => {
+  if (
+    "ogTitle" in result &&
+    typeof result.ogTitle === "string" &&
+    "ogDescription" in result &&
+    typeof result.ogDescription === "string" &&
+    "ogImage" in result &&
+    typeof result.ogImage === "object" &&
+    result.ogImage !== null &&
+    "url" in result.ogImage &&
+    typeof result.ogImage.url === "string"
+  ) {
+    return true;
+  }
+
+  return false;
 };
 
 export const extractCardData = async (url: string): Promise<CardData | undefined> => {
-  const data: { [key: string]: string | undefined } = {};
   try {
-    // @ts-ignore: 型が間違っている
     const ogsData = await ogs({ url, downloadLimit: 1000000 * 20 });
-    // @ts-ignore: 型が間違っている
-    const { result } = ogsData as SuccessResult;
-    data.title = result.ogTitle;
-    data.description = result.ogDescription;
-    // ここは型が複雑なので、キャストした
-    data.image = (result.ogImage as OpenGraphImage).url;
+    const { result } = ogsData;
+    if (!isOgsResultType(result)) {
+      throw new Error("Invalid result type.");
+    }
+
     const hostname = new URL(url).hostname;
-    data.hostname = hostname;
-    data.favicon = `https://www.google.com/s2/favicons?domain=${hostname}`;
-    data.url = url;
+    return {
+      title: result.ogTitle,
+      description: result.ogDescription,
+      image: result.ogImage.url,
+      hostname,
+      favicon: `https://www.google.com/s2/favicons?domain=${hostname}`,
+      url,
+    };
   } catch (e) {
     return undefined;
   }
-
-  if (!cardDataValidator(data)) {
-    return undefined;
-  }
-
-  return data;
 };
