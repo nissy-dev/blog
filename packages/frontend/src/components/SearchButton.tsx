a0b16dd7ff8f3e2dd09cc5a87ef7106b5946dfab
nissy-dev <nd.12021218@gmail.com>
2024-08-02 01:03:42 +0900

    finish fts and search ui

--- packages/frontend/src/components/SearchButton.tsx
+++ packages/frontend/src/components/SearchButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import {
   type ChangeEvent,
   type MouseEvent,
@@ -21,27 +22,29 @@ export const SearchButton = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const deferredQuery = useDeferredValue(searchQuery);
 
-  const handleSearchButton = () => dialogRef.current?.showModal();
-  const handleSearchBox = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e.target.value);
-  }, []);
   const onClickDialogBackDrop = useCallback(
     (e: MouseEvent<HTMLDialogElement>) => {
       if (
         e.target instanceof HTMLDialogElement &&
         e.target.closest("#dialog-container") === null
       ) {
+        setSearchQuery("");
         dialogRef.current?.close();
       }
     },
     [],
   );
 
+  const onClickLink = useCallback(() => {
+    setSearchQuery("");
+    dialogRef.current?.close();
+  }, []);
+
   return (
     <>
       <button
         type="button"
-        onClick={handleSearchButton}
+        onClick={() => dialogRef.current?.showModal()}
         className={styles.button}
         title={t("search-blog")}
       >
@@ -53,16 +56,80 @@ export const SearchButton = () => {
         className={styles.dialog}
         onClick={onClickDialogBackDrop}
       >
-        <div id="dialog-container">
+        <div id="dialog-container" className={styles.dialogContainer}>
           <input
             type="search"
-            id="search"
             placeholder={t("search-blog")}
             value={searchQuery}
-            onChange={handleSearchBox}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setSearchQuery(e.target.value)
+            }
+          />
+          <SearchResults
+            searchQuery={deferredQuery}
+            onClickLink={onClickLink}
           />
         </div>
       </dialog>
     </>
+  );
+};
+
+type Post = {
+  id: string;
+  title: string;
+  content: string;
+};
+
+type SearchResultProps = {
+  searchQuery: string;
+  onClickLink: () => void;
+};
+
+const FTS_API_ENDPOINT =
+  process.env.NODE_ENV === "production" ? "" : "http://localhost:8787";
+
+const SearchResults = ({ searchQuery, onClickLink }: SearchResultProps) => {
+  if (searchQuery === "") {
+    return null;
+  }
+
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    async function fetchSearchResults() {
+      const response = await fetch(
+        `${FTS_API_ENDPOINT}/api/fts/search?q=${searchQuery}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+      const result = (await response.json()) as { posts: Post[] };
+      setPosts(result.posts);
+    }
+
+    fetchSearchResults();
+  }, [searchQuery]);
+
+  return (
+    <div className={styles.searchResult}>
+      {posts.length === 0 && <div>No results for "{searchQuery}"</div>}
+      {posts.length > 0 && (
+        <ul>
+          {posts.map((post) => {
+            return (
+              <li key={post.id}>
+                <Link href={`/post/${post.id}`} onClick={onClickLink}>
+                  {post.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
   );
 };
