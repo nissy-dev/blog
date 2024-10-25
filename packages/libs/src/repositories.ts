e7400cc895c47b5d26dedf152b913a31f33eeb65
nissy-dev <nd.12021218@gmail.com>
2024-10-25 23:51:06 +0900

    fix typecheck

--- packages/libs/src/repositories.ts
+++ packages/libs/src/repositories.ts
@@ -1,3 +1,4 @@
 export { getFrontMatters } from "./repositories/getFrontMatters";
 export { getPostIds } from "./repositories/getPostIds";
 export { getPostById } from "./repositories/getPostById";
+export { getTags } from "./repositories/getTags";
